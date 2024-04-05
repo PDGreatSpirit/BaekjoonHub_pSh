1f7e36d95849f84d274780a8b1eac376e3f75792
PDGreatSpirit <159146540+PDGreatSpirit@users.noreply.github.com>
2024-04-05 10:32:52 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1328 ms, Memory: 79156 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Main {
 
     static int[] parents;
-    static ArrayList<Integer>[] arr;
+    static ArrayList<Integer>[] node;
 
     static boolean[] isVisit;
 
@@ -17,29 +17,29 @@ public class Main {
         BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         parents= new int[n+1];
-        arr = new ArrayList[n+1];
+        node = new ArrayList[n+1];
         isVisit = new boolean[n+1];
         for (int i = 0; i < n+1; i++) {
-            arr[i] = new ArrayList<>();
+            node[i] = new ArrayList<>();
         }
         for (int i = 0; i < n-1; i++) {
             StringTokenizer st2 = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st2.nextToken());
             int y = Integer.parseInt(st2.nextToken());
-            arr[x].add(y);
-            arr[y].add(x);
+            node[x].add(y);
+            node[y].add(x);
         }
         dfs(1);
         for (int i = 2; i < parents.length; i++) {
             System.out.println(parents[i]);
         }
     }
-    public static void dfs(int n){
-        isVisit[n] = true;
-        for(int i : arr[n]){
-            if(!isVisit[i]){
-                parents[i] = n;
-                dfs(i);
+    public static void dfs(int index){
+        isVisit[index] = true;
+        for(int node : node[index]){
+            if(!isVisit[node]){
+                parents[node] = index;
+                dfs(node);
             }
         }
     }
