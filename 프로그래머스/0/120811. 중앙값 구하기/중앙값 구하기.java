ba4d98530b544cf261d48475c587d29cce134040
PDGreatSpirit <159146540+PDGreatSpirit@users.noreply.github.com>
2024-04-11 10:46:23 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.34 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        int x = array.length/2;
+        
+        return array[x];
+    }
+}
