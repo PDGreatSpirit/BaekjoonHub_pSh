6f962e007b667ba4af877c76ae30fe153fe5626f
PDGreatSpirit <159146540+PDGreatSpirit@users.noreply.github.com>
2025-02-04 15:17:14 +0900

    [Bronze IV] Title: 베라의 패션, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon s®rc="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n*(n-1) );
+    }
+}
+
