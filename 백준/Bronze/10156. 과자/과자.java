2eb5d8584b8536a0bc78ab9c253217baddb29ba7
PDGreatSpirit <159146540+PDGreatSpirit@users.noreply.github.com>
2025-03-07 13:05:22 +0900

    [Bronze IV] Title: 과자, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon s®rc="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int z = Integer.parseInt(st.nextToken());
+        int answer = x*y - z;
+        System.out.println(Math.max(answer, 0));
+    }
+}
+
