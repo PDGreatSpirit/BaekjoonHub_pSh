fb2c8b84cbaeea00d84668315ddb267ece37bb28
PDGreatSpirit <159146540+PDGreatSpirit@users.noreply.github.com>
2025-03-07 13:08:41 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon s®rc="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int z = Integer.parseInt(br.readLine());
+        if((x+y) < z*2){
+            System.out.println(x+y);
+        }
+        else {
+            System.out.println((x+y)-2*z);
+        }
+    }
+}
+
