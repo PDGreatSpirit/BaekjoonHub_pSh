c09e49978146a846aa0d0998185fb139735384b6
PDGreatSpirit <159146540+PDGreatSpirit@users.noreply.github.com>
2025-03-28 15:15:22 +0900

    [Bronze IV] Title: Hello Judge, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon s®rc="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int  n= Integer.parseInt(br.readLine());    
+        for (int i = 0; i <n ; i++) {
+            StringBuilder  sb = new StringBuilder("Hello World, Judge ");
+            sb.append(i+1);
+            sb.append("!");
+            System.out.println(sb);
+
+        }
+    }
+}
+
