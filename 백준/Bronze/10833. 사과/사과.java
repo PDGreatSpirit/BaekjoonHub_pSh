619729bbc8d6f480aa82b0a4da5a0fe1ea0cd57c
PDGreatSpirit <159146540+PDGreatSpirit@users.noreply.github.com>
2025-03-28 15:19:49 +0900

    [Bronze III] Title: 사과, Time: 112 ms, Memory: 14396 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon s®rc="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int  n= Integer.parseInt(br.readLine());
+        int an = 0 ;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x  = Integer.parseInt(st.nextToken());
+            int y  = Integer.parseInt(st.nextToken());
+            an += y%x;
+        }
+        System.out.println(an);
+    }
+}
+
